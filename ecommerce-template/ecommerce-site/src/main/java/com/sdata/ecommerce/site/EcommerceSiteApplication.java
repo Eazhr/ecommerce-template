6bb82b11bc54e29d1a6f4081668e4aa2e619b2d3
ned <ned@igola.com>
2019-02-12 14:01:27 +0800

    First commit.

---
+++ ecommerce-template/ecommerce-site/src/main/java/com/sdata/ecommerce/site/EcommerceSiteApplication.java
@@ -0,0 +1,14 @@
+package com.sdata.ecommerce.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceSiteApplication.class, args);
+    }
+
+}
+
